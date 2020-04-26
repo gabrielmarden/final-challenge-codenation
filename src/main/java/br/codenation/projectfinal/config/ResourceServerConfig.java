c28cd7ea1f04a834b9aaf68554d68bc38ad7106e
Marden Rocha <mardenbiel@gmail.com>
2020-04-26 18:24:38 -0300

    CORS configured

--- src/main/java/br/codenation/projectfinal/config/ResourceServerConfig.java
+++ src/main/java/br/codenation/projectfinal/config/ResourceServerConfig.java
@@ -5,13 +5,17 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.cors()
+                .configurationSource(request->new CorsConfiguration().applyPermitDefaultValues())
+                .and()
+                .authorizeRequests()
                 .antMatchers(HttpMethod.GET,"/user").authenticated()
                 .antMatchers(HttpMethod.GET,"/evento").authenticated();
     }
