9b8988c95da72dff7768606476220dc76c261d48
Marden Rocha <mardenbiel@gmail.com>
2020-04-30 00:21:02 -0300

    Add authentication options in the swagger docs

--- src/main/java/br/codenation/projectfinal/swagger/SwaggerConfig.java
+++ src/main/java/br/codenation/projectfinal/swagger/SwaggerConfig.java
@@ -4,17 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.builders.*;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -26,14 +24,9 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.codenation.projectfinal"))
                 .build()
-                .globalOperationParameters(Collections.singletonList(new ParameterBuilder()
-                    .name("Authorization")
-                    .description("Bearer token")
-                    .modelRef(new ModelRef("String"))
-                    .parameterType("header")
-                    .required(true)
-                    .build()))
-                .apiInfo(metaData());
+                .apiInfo(metaData())
+                .securitySchemes(Collections.singletonList(oauth()))
+                .securityContexts(Collections.singletonList(securityContext()));
 
     }
 
@@ -56,4 +49,45 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
+
+    @Bean
+    List<GrantType> grantTypes(){
+        List<GrantType> grantTypes = new ArrayList<>();
+        grantTypes.add(new ResourceOwnerPasswordCredentialsGrant("https://central-de-logs-api.herokuapp.com/oauth/token"));
+        return grantTypes;
+    }
+
+    @Bean
+    SecurityScheme oauth(){
+        return new OAuthBuilder()
+                .name("OAuth2")
+                .scopes(scopes())
+                .grantTypes(grantTypes())
+                .build();
+    }
+
+    private List<AuthorizationScope> scopes(){
+        List<AuthorizationScope> list = new ArrayList<>();
+        list.add(new AuthorizationScope("read","Grant read access"));
+        list.add(new AuthorizationScope("write","Grant write access"));
+        list.add(new AuthorizationScope("trust","Grant read write and delete access"));
+        return list;
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.ant("/user/**"))
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[2];
+
+        authorizationScopes[0] = new AuthorizationScope("read","Grant read access");
+        authorizationScopes[1] = new AuthorizationScope("read","Grant write access");
+
+        return Collections.singletonList(new SecurityReference("Oauth2Scheme",authorizationScopes));
+    }
+
 }
