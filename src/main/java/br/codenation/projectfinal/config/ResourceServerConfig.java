0d473e95adc7a05075b9eb8077d6ca31a9c0a4f5
Marden Rocha <mardenbiel@gmail.com>
2020-05-03 16:04:26 -0300

    Fix CORS issues

--- src/main/java/br/codenation/projectfinal/config/ResourceServerConfig.java
+++ src/main/java/br/codenation/projectfinal/config/ResourceServerConfig.java
@@ -1,11 +1,16 @@
 package br.codenation.projectfinal.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
@@ -13,10 +18,20 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.cors()
-                .configurationSource(request->new CorsConfiguration().applyPermitDefaultValues())
+                .configurationSource(corsConfigurationSource())
                 .and()
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
